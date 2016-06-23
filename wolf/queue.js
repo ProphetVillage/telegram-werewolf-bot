2c715e11640ff5d8909749ecea89ad1d5b42bd50
tengattack <tengattack@foxmail.com>
2016-06-23 21:45:36 +0800

    add Role & EventQueue

---
+++ wolf/queue.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function EventQueue() {
+
+}
+
+module.exports = EventQueue;
--- wolf/roles/role.js
+++ wolf/roles/role.js
@@ -2,6 +2,7 @@
 
 function Role(wolf, player) {
   this.wolf = wolf;
+  this.chat_id = wolf.chat_id;
   this.ba = wolf.ba;
   this.player = player;
 
@@ -9,16 +10,63 @@ function Role(wolf, player) {
   this.name = '';
   this.id = '';
   this.dead = false;
+
+  // event done flag, refresh every event time
+  this.done = false;
+  this.allowEvents = []; // allowed events' name, such as 'kill'
 }
 
 Role.prototype.eventDay = function () {
-
+  if (!this.done) {
+    // check event done
+    this.done = true;
+  }
 };
 
 Role.prototype.eventDusk = function () {
-
+  if (!this.done) {
+    // check event done
+    this.done = true;
+  }
 };
 
 Role.prototype.eventNight = function () {
+  if (!this.done) {
+    // check event done
+    this.done = true;
 
+    // current waiting time queue.getTime();
+    // such as
+    /*var self = this;
+    var timer = setTimeout(() => {
+      if (!self.done) {
+
+      }
+    }, queue.getTime());
+    // player
+    ba.sendMessage({
+      chat_id: this.chat_id,
+      text: msg,
+      reply_markup: JSON.stringify({
+        inline_keyboard: [ [ { text: 'Player 1', callback_data: '/kill ' + player1.id },
+          { text: 'Player 2', callback_data: '/kill ' + player2.id }] ]
+      }),
+    }, (err, r) {
+      if (err) console.log(err);
+    }); */
+  }
+};
+
+Role.prototype.eventDayCallback = function (queue) {
+  this.done = true;
+};
+
+Role.prototype.eventDuskCallback = function (queue) {
+  this.done = true;
+};
+
+Role.prototype.eventNightCallback = function (queue) {
+  this.done = true;
+  // TODO: add to queue
+  // queue.add('kill', whom);
 };
