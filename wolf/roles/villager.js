b46c0429435b3fed85e15600a3b7a52110c9c2dc
Rui <rui@su.ki>
2016-07-08 14:00:50 +0800

    add villager

---
+++ wolf/roles/villager.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const Role = require('./role');
+
+class Villager extends Role {
+  constructor(villager, player) {
+    super(villager, player);
+
+    this.id = 'villager';
+    this.name = 'Villager';
+    this.priority = 0;
+
+    this.allowEvents = [''];
+  }
+
+  eventAnnouncement() {
+    this.ba.sendMessage({
+      chat_id: this.user_id,
+      text: 'You are a villager.',
+    }, (err, r) {
+      if (err) console.log(err);
+    });
+  }
+}
