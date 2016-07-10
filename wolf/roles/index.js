2ea2579eb9ca6e6b741953b1a3fca6639771a1b1
Rui <rui.github@gmail.com>
2016-07-10 18:07:49 +0800

    update rules for test

--- wolf/roles/index.js
+++ wolf/roles/index.js
@@ -4,20 +4,38 @@ var Roles = {
   // list of roles
   Villager: require('./villager'),
   Wolf: require('./wolf'),
-}; 
+  Prophet: require('./prophet'),
+  Fool: require('./fool')
+};
 
 exports.Roles = Roles;
 
 // list of Roles' id
-exports.role_list = [ 'villager', 'wolf' ];
-exports.event_list = [ 'vote', 'bite' ];
+var role_list = [ 'villager', 'wolf', 'prophet', 'fool' ];
+exports.role_list = [ 'villager', 'wolf', 'prophet', 'fool' ];
+exports.event_list = [ 'vote', 'bite', 'know' ];
 
 exports.setRandomRoles = function (wolf, players) {
   // TODO: set player role here
   // for test
-  players[0].role = new Roles.Wolf(wolf, players[0]);
-  if (players.length > 1) {
-    players[1].role = new Roles.Wolf(wolf, players[1]);
+  let list = role_list;
+  for (let player of players) {
+    let role = list[Math.floor(Math.random() * list.length)];
+    switch (role) {
+      case 'wolf':
+        player.role = new Roles.Wolf(wolf, player);
+        break;
+      case 'prophet':
+        player.role = new Roles.Prophet(wolf, player);
+        list.splice(list.indexOf('prophet'));
+        break;
+      case 'fool':
+        player.role = new Roles.Fool(wolf, player);
+        list.splice(list.indexOf('fool'));
+        break;
+      default:
+        player.role = new Roles.Villager(wolf, player);
+    }
   }
 };
 
