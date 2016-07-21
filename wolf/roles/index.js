e3471ba47f727bf2fde5df69908acbe30889e088
tengattack <tengattack@foxmail.com>
2016-07-21 21:26:47 +0800

    update random algo

--- wolf/roles/index.js
+++ wolf/roles/index.js
@@ -88,7 +88,11 @@ exports.setRandomRoles = function (wolf, players) {
   };
 
   if (player_count < 7) {
-    other_list.wolf = 1;
+    if (getRandom(1, 0)) {
+      other_list.wolf = 1;
+    } else {
+      other_list.partymember = 1;
+    }
     other_list.villager = 1;
     other_list.mason = getRandom(2, 0);
   } else if (player_count < 10) {
